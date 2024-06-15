e2238bb5a8e7a334c6c5997aeb62a8811d470cee
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-06-15 17:13:59 +0200

    Added Routing structure and logos

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className='mt-9 flex-col ga-9'>
+            <section className='flex flex-col gap-5'>
+                <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+            </section>
+        </div>
+    );
+}
+
+
+export default Page;
