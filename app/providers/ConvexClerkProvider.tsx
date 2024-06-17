09faae365cefc00beadfda3b87f986903e14a074
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-06-18 01:39:34 +0200

    Added Clerk Authentication

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -2,14 +2,26 @@
 
 import React, { ReactNode } from "react";
 
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 
 const convex: ConvexReactClient = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string} appearance={{
+        layout: {
+            socialButtonsVariant: 'iconButton',
+            logoImageUrl: 'logo-icon.svg'
+        },
+        variables: {
+            colorBackground: '#15171c',
+            colorPrimary: '',
+            colorText: 'white',
+            colorInputBackground: '#1b1f29',
+            colorInputText: 'white',
+        }
+    }}>
         <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
             {children}
         </ConvexProviderWithClerk>
--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,8 +1,9 @@
+/*
 import { query } from "./_generated/server";
 
 export const get = query({
     args: {},
     handler: async(ctx) => {
-        return await ctx.db.query("tasks").collect();
+        return await ctx.db.query('tasks').collect();
     },
-});
+});*/
